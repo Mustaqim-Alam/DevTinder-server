0eac5bf4fcff6194876816aa48410a580320c52e
Md Mustaqim Alam <mustaqimalam@gmail.com>
2026-02-02 22:52:49 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -5,9 +5,18 @@ const APP_PORT = process.env.APP_PORT || 3000;
 
 const app = express();
 
-app.use('/', (req,res)=>{
-    res.send("DevTinder Backend is running....")
-})
+app.use("/admin", (req, res, next) => {
+  let token = "admin";
+  const isAuth = token === "admin";
+  if (!isAuth) {
+    return res.status(403).send("Access denied for non-admin users");
+  }
+  next();
+});
+
+app.get("/admin", (req, res) => {
+  res.send("All Data for Admin");
+});
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port ${APP_PORT}`);
